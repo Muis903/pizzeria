a2e08840455035b4574602910680f7590766ba05
moonwake769 <86232674+moonwake769@users.noreply.github.com>
2021-10-22 17:55:40 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,18 +1,1 @@
-#Deze is aangemaakt voor de uitbereiding van de main.py code
-
-pizza_0 = {'product': "Pizza Hawai", 'price': "11"}
-pizza_1 = {'product': "Pizza Salami", 'price': "11"}
-pizza_2 = {'product': "Pizza 4 Kazen", 'price': "10"}
-pizza_3 = {'product': "Pizza Vegetarisch", 'price': "10"}
-pizza_4 = {'product': "Pizza Margharita", 'price': "9"}
-pizza_5 = {'product': "Pizza Zeewereld", 'price': "12"}
-
-
-drank_0 = {'product': "Cola 0,33", 'price': "2"}
-drank_1 = {'product': "Fanta 0,33", 'price': "2"}
-drank_2 = {'product': "7 UP 0,33", 'price': "2"}
-drank_3 = {'product': "Spa water 0,5", 'price': "1"}
-
-
-menu = [pizza_0, pizza_1, pizza_2, pizza_3, pizza_4, pizza_5, drank_0, drank_1, drank_2, drank_3]
 
