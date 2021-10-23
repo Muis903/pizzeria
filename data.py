58799e403d56e9b452997b6416eb4b4e22be2e0a
Muis903 <81823117+Muis903@users.noreply.github.com>
2021-10-23 22:38:09 +0200

    New function to add/save new staff

--- data.py
+++ data.py
@@ -5,12 +5,38 @@
 
 import ast
 import json
-import yaml
-
 
 #
 # DEFINING functions.
 #
+
+def add_staff():
+    """Add new staff and save it in the list."""
+
+    #Data
+    name = input("First and last name: ")
+    birthday = input("Birthday dd/mm/yyyy: ")
+    login = input("Login: ")
+    password = input("Password: ")
+    function = input("Function: ")
+
+    #Create profile
+    profile = str("\n" + "Name: " + name.title() + "\n"
+        + "Birthday: " + "\n" + "Login: " + login + "\n" + "Password: " + password + "\n" + "Function: " + function)
+    return profile
+
+    #Add and save profile
+    try:
+        with open ('data.json', "r+") as data:
+            if profile in data:
+                pass
+            else:
+                json.dump(profile, data)
+    except FileNotFoundError:
+        with open ('data.json', "w") as data:
+            json.dump(profile, data)
+
+
 
 
 def save_order(order_id, order):
@@ -19,3 +45,10 @@ def save_order(order_id, order):
     order_id_to_order = {order_id: order}
     print(order_id_to_order)
     print("The new order is successfully saved!")
+
+
+#
+# <--- Test Function --->
+#
+
+add_staff()
