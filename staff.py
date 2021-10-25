bbda24abad468cdf8650d6d3bdb1a0ea9899384e
moonwake769 <exhilaratedreverie288@protonmail.ch>
2021-10-25 18:59:33 +0200

    Update

--- staff.py
+++ staff.py
@@ -22,7 +22,7 @@ staff_json = "staff.json"
 
 
 def get_new_person_profile() -> dict: # dat de functie retourneert dict (voor leesbaarheid bedoeld). Kan ook niet gebruiken. Verwirder deze omschrijving als duidelijk.
-    """Get a new staff profile and save it into the staff.json."""
+    """Get a new person profile and save it into the staff.json."""
 
     #Data
     name = input("First and last name: ")
@@ -31,12 +31,13 @@ def get_new_person_profile() -> dict: # dat de functie retourneert dict (voor le
     password = input("Password: ")
     position = input("Position: ")
     
-    #Create an object as a staff profile in the form of Python dictionary.
+    #Create an object as a person profile in the form of Python dictionary.
     person_profile = create_person_profile(name, birthday, login, password, position)
-    print("Created a new staff profile.")
+    print("Created a new person profile.")
 
     # Saving new staff.
     save_new_person_profile(person_profile, position)
+    print("The new person profile is successfully added into staff.json :)")
 
     # Returning the person_profile to work with further if need it.
     return person_profile
@@ -50,34 +51,32 @@ def get_staff_positions() -> list:
     positions = [
         "junior", "medio", "senior", "team leader", "project leader", "manager"
     ]
-    print("Checking for all positions.")
 
     return positions
 
 
 def save_new_person_profile(person_profile:dict, position:str): # :dict geeft aan dat de attribuut dict MOET zijn. Er zijn nog :str, :list ezv. Verwijder de omschrijving als duidelijk ;-)
-    """Save new staff profile in staff.json file in json file format."""
+    """Save new person profile in staff.json file in json file format."""
 
     # Create staff.json file is the file does not exist.
     create_staff_json_file()
     # Create staff.json template if the staff.json file is empty.
     create_staff_json_template()
 
-    # Putting the staff profile into the staff.json.
+    # Putting the person profile into the staff.json.
     with open(staff_json, 'r+') as file:
         # First we loading an existing staff into a Python dict.
         file_data = json.load(file)
-        # Joining staff profile with file_data inside position dict.
+        # Joining person profile with file_data inside position dict.
         file_data[position].append(person_profile)
         # Sets file's current position at offset.
         file.seek(0)
-        # Dumping the staff profile to the file with indent of 4 spaces.
+        # Dumping the person profile to the file with indent of 4 spaces.
         json.dump(file_data, file, indent = 4)
-        print("The new staff profile is successfully added into staff.json :)")
 
 
 def create_person_profile(name=None, birthday=None, login=None, password=None, position=None): # None betekent dat de waarde van de parameter is niet vereist. Verwijder de omschrijving als duidelijk :)
-    """Create and return a staff profile."""
+    """Create and return a person profile."""
     
     profile = {
         "name": name,
@@ -102,7 +101,7 @@ def create_staff_json_file():
         open(staff_json, 'w')
         print("Created new staff.json file.")
     else:
-        print("staff.json file already exists.")
+        print("Checked: found staff.json.")
 
 
 def staff_json_file_is_empty():
@@ -134,12 +133,12 @@ def create_staff_json_template():
     """
     
     if staff_json_file_is_empty() == False:
-        print("staff.json is not empty.")
+        print("Checked: staff.json is not empty.")
     else:
         print("Creating a template in staff.json")
         # Getting all positions in the company.
         staff_positions = get_staff_positions()
-        # Defining the staff profile as Python dictionary.
+        # Defining the person profile as Python dictionary.
         person_profile = dict()
         # Looping through all positions and adding to the template.
         for position in staff_positions:
